d053eb02cfe25c1faf2c176861484c6ddfc861e6
StephenAraka <stevenaraka@gmail.com>
2020-05-03 18:00:57 +0300

    add routing

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import { Router, Route } from 'react-router-dom';
+import { history } from '../store/histoy';
+
 import Dashboard from './Dashboard';
 
 export const Main = () => (
-  <Provider store={store} >
-    <div>
-      <Dashboard />
-    </div>
-  </Provider>
+  <Router history={history}>
+    <Provider store={store} >
+      <Route
+        exact
+        path="/dashboard"
+        render={() => <Dashboard />}
+      />
+    </Provider>
+  </Router>
 
 )
